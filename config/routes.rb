c203a02c3a8b32ebf1f27bbaf064221b578385ab
hado anselme <anselmehado85@yahoo.com>
2021-06-12 23:18:13 +0100

    oki

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute"
-  #root "posts#index"
-  resources :posts
+  #post "/graphql", to: "graphql#execute"
+  post '/graphql', to: 'graphql#execute'
+
+if Rails.env.development?
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+
+  root to: redirect('/graphiql')
+end
+ resources :posts
 end
