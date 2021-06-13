66e69d3a564004354c30bc7cc3bf922923a1180d
hado anselme <anselmehado85@yahoo.com>
2021-06-13 10:51:12 +0100

    graphok

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -2,5 +2,5 @@ Types::PostType = GraphQL::ObjectType.define do
   name "Post"
   field :id, !types.ID
   field :title, !types.String
-  field :text, !types.String
+  field :content, !types.String
 end
